d44dc5373ecfa29855f9ec77df71e629c5dc7428
LmLilly <lillym@Lidiettes-MacBook-Pro.local>
2017-05-06 09:18:36 -0700

    Turned in changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,14 +5,20 @@ $(document).ready(function(){
 	//players random nuber 19-120
 	randomNum = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
 	$(".randomNumber").html(randomNum);
-	//crystals random number 1-12
-
-
+	
+	
 	//My variables
-	var numberOptions = [10, 5, 3, 1];
+	//var numberOptions = [10, 5, 3, 1];
 	var ongoingScore = 0
 	var loses = 0
 	var wins = 0
+	
+	var numberOptions = [Math.floor(Math.random()*(12 - 1) + 1), 
+		Math.floor(Math.random()*(12 - 1) + 1), 
+		Math.floor(Math.random()*(12 - 1) + 1), 
+		Math.floor(Math.random()*(12 - 1) + 1)]
+	console.log(numberOptions);
+
 	
 
      $(".crystal").on("click", function(){
@@ -22,15 +28,16 @@ $(document).ready(function(){
  		ongoingScore = ongoingScore + myScore
  		console.log(ongoingScore);
 
-	$(".score").html(ongoingScore);
+	 $(".score").html(ongoingScore);
 
 	if (ongoingScore > randomNum){
 	//User loses
-		loses = loses + 1
+			loses = loses + 1
 		$(".Loses").html("Loses: " + loses);
-
+		
 		for(var i = 0; i < numberOptions.length; i++){
 			numberOptions[i] = Math.floor(Math.random()*26);
+		
 		}
 
 		randomNum = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
@@ -38,11 +45,13 @@ $(document).ready(function(){
 		
 		ongoingScore  = 0
 		$(".score").html(ongoingScore);
-	}
+
+		}
 	else if (ongoingScore === randomNum){
 		
 			wins = wins + 1
 		$(".Wins").html("Wins: " + wins);
+
 	
 		for(var i = 0; i < numberOptions.length; i++){
 			numberOptions[i] = Math.floor(Math.random()*26);
